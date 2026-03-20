0edbf5892e6e5caa5378ae7d3193dda4fc3637ee
WarriorHanamy <632029496@qq.com>
2026-03-20 13:46:37 +0800

    update

--- simple_px4_odom/src/simple_px4_odom/src/mocap_to_mavros_odom.cpp
+++ simple_px4_odom/src/simple_px4_odom/src/mocap_to_mavros_odom.cpp
@@ -1,7 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Odometry.h>
-#include <Eigen/Geometry>
 #include <limits>
 #include <mutex>
 
@@ -23,9 +22,6 @@ private:
   geometry_msgs::PoseStamped latest_pose_;
   bool has_data_;
   std::mutex mutex_;
-
-  static const Eigen::Quaterniond q_enu_ned;
-  static const Eigen::Quaterniond q_flu_frd;
 
   void cb(const geometry_msgs::PoseStamped::ConstPtr& msg) {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -56,12 +52,9 @@ private:
       pz /= 1000.0;
     }
 
-    Eigen::Vector3d p_enu(px, py, pz);
-    Eigen::Vector3d p_ned = q_enu_ned * p_enu;
-
-    out.pose.pose.position.x = p_ned.x();
-    out.pose.pose.position.y = p_ned.y();
-    out.pose.pose.position.z = p_ned.z();
+    out.pose.pose.position.x = px;
+    out.pose.pose.position.y = py;
+    out.pose.pose.position.z = pz;
 
     double nan_val = std::numeric_limits<double>::quiet_NaN();
     out.twist.twist.linear.x = nan_val;
@@ -71,25 +64,11 @@ private:
     out.twist.twist.angular.y = nan_val;
     out.twist.twist.angular.z = nan_val;
 
-    Eigen::Quaterniond q_enu_flu(msg.pose.orientation.w,
-                                  msg.pose.orientation.x,
-                                  msg.pose.orientation.y,
-                                  msg.pose.orientation.z);
-    q_enu_flu.normalize();
-    Eigen::Quaterniond q_ned_frd = q_enu_ned * q_enu_flu * q_flu_frd;
-    q_ned_frd.normalize();
-
-    out.pose.pose.orientation.w = q_ned_frd.w();
-    out.pose.pose.orientation.x = q_ned_frd.x();
-    out.pose.pose.orientation.y = q_ned_frd.y();
-    out.pose.pose.orientation.z = q_ned_frd.z();
+    out.pose.pose.orientation = msg.pose.orientation;
 
     pub_.publish(out);
   }
 };
-
-const Eigen::Quaterniond MocapToMavrosOdom::q_enu_ned(0.0, M_SQRT1_2, M_SQRT1_2, 0.0);
-const Eigen::Quaterniond MocapToMavrosOdom::q_flu_frd(0.0, 1.0, 0.0, 0.0);
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "mocap_to_mavros_odom");
